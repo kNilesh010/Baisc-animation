2ef28258f8c3e2de6d94a6be818c585c9268be93
Nilesh Kumar <k.nilesh010@gmail.com>
2022-04-10 11:39:36 +0530

    final commit

--- Basic animation/ViewController.swift
+++ Basic animation/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var myView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        viewDidLayoutSubviews()
     }
 
+    override func viewDidLayoutSubviews() {
+        
+        myView.backgroundColor = .red
+        myView.center = view.center
+        view.addSubview(myView)
+        
+        let myButton = UIButton(frame: CGRect(x: (view.frame.size.width - 200) / 2, y: view.frame.size.width - 200, width: 100, height: 20))
+        myButton.backgroundColor = .blue
+        //myButton.titleColor(for: UIColor.white)
+        myButton.setTitle("Animate", for: .normal)
+        myButton.addTarget(self, action: #selector(Animate), for: .touchUpInside)
+        view.addSubview(myButton)
+    }
+    
+    @objc func Animate(){
+        UIView.animate(withDuration: 1) {
+            self.myView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+        }
+    }
 
 }
 
